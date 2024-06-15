c3775ed8b95cd7e50dac0bf3134ed9815383e569
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2024-06-15 20:49:06 +0700

    used useSelector and useDispatch

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,26 +1,20 @@
 import { connect } from "react-redux"
-import * as actions from "../actions";
-import { bindActionCreators } from "redux";
+import {inc, dec, rnd} from "../actions";
+import { useSelector, useDispatch } from "react-redux";
 
-const Counter = ({counter, inc, dec, rnd}) => {
+const Counter = () => {
+
+    const counter = useSelector(state => state.counter);
+    const dispatch = useDispatch();
+
     return (
         <div id="" className="jumbotron">
             <h1 className="counter">{counter}</h1>
-            <button onClick={inc} className="btn btn-primary">INC</button>
-            <button onClick={dec} className="btn btn-primary">DEC</button>
-            <button onClick={rnd} className="btn btn-primary">RND</button>
+            <button onClick={() => dispatch(inc())} className="btn btn-primary">INC</button>
+            <button onClick={() => dispatch(dec())} className="btn btn-primary">DEC</button>
+            <button onClick={() => dispatch(rnd())} className="btn btn-primary">RND</button>
         </div>
     )
 }
 
-const mapStateToProps = (state) => {
-    return {
-        counter: state.value
-    }
-}
-
-// const mapDispatchToProps = (dispatch) => {
-//     return bindActionCreators(actions, dispatch);
-// }
-
-export default connect(mapStateToProps, actions)(Counter)
+export default Counter
